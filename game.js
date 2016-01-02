918f070b16a0f6b1083b0d7c723fedc2b98e2ce1
trebek1 <trebek08@gmail.com>
2016-01-02 15:29:33 -0800

    Ball bounces off the wall

--- game.js
+++ game.js
@@ -3,8 +3,10 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
-var dy = -2;	// 20 from the left, 40 from the top, 50 wide, 50 high 
+var dy = -2;	
+var ballRadius = 10; 
 
+	// 20 from the left, 40 from the top, 50 wide, 50 high 
 	// ctx.rect(20, 40, 50, 50);
 	// ctx.fillStyle = "#FF0000";
 	// ctx.fill();
@@ -26,7 +28,7 @@ var dy = -2;	// 20 from the left, 40 from the top, 50 wide, 50 high
 	
 	function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -35,10 +37,22 @@ var dy = -2;	// 20 from the left, 40 from the top, 50 wide, 50 high
 	
 	function draw() {
     	
-    	ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		drawBall(); 
 		x+= dx; 
 		y+= dy; 
+
+		//Top and Bottom edges, counting starts at top left corner 
+		if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+			dy = -dy
+		}
+
+		if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+	    	dx = -dx;
+		}
 	}
+	
+	
+	
 
 setInterval(draw, 10);
